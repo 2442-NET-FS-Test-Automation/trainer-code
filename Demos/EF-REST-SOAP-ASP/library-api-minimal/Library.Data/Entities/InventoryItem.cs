e4f4ed20d2502b22e2db1eefba0469e82a126ba8
jdelacruz96 <jonathan.delacruz@revature.com>
2026-07-02 09:16:00 -0600

    test

--- Demos/EF-REST-SOAP-ASP/library-api-minimal/Library.Data/Entities/InventoryItem.cs
+++ Demos/EF-REST-SOAP-ASP/library-api-minimal/Library.Data/Entities/InventoryItem.cs
@@ -2,7 +2,7 @@ namespace Library.Data.Entities;
 
 public class InventoryItem 
 {
-    public int Id { get; set; }
+    public int Id { get; set;  }
     public int ProductId { get; set; } // FK - 1:1 with product
     public Product Product { get; set; } = default!; // we can have EF give a default value 
     public int CurrentStock { get; set; } // how many of this thing do we have
